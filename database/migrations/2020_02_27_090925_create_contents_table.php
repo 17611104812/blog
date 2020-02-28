0392063fc535c0c6a9ca40a21911a0deec4ee536
吕向杰 <v-xiangjielv@creditease.cn>
2020-02-28 11:08:38 +0800

    1

--- database/migrations/2020_02_27_090925_create_contents_table.php
+++ database/migrations/2020_02_27_090925_create_contents_table.php
@@ -14,7 +14,7 @@ class CreateContentsTable extends Migration
     public function up()
     {
         //
-        Schema::create('comment', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('post_id')->default(0);
             $table->integer('user_id')->default(0);
